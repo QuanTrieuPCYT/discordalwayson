98f9da8e264c382b54f5417d99e2da8cd6268c4e
QuanTrieuPCYT <trieuq4@outlook.com>
2021-05-16 00:31:59 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord, os, keep_alive
+
+from discord.ext import tasks, commands
+
+client = commands.Bot(
+  command_prefix=':',
+  self_bot=True
+)
+
+@client.event
+async def on_connect():
+  await client.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.watching, name="Amelia's stream 'till I'm offline."))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+client.run(os.getenv("TOKEN"), bot=False)
